fe60ede244943b4d84165186ad4657576f214818
fsanchis <sanchis.floriane@live.fr>
2018-05-02 14:47:11 +0200

    Commit page ressource + menu

--- View/test.php
+++ View/test.php
@@ -18,8 +18,6 @@ if (isset($_POST['creer']))
 <body>
 
 <form action="test.php" method="post">
-    Name: <input type="text" name="name"><br>
-    E-mail: <input type="text" name="email"><br>
     <input type="submit" name="creer">
 </form>
 
